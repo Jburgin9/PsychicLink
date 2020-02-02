9a0ad706ae3fc12982ece8c32bdc76ec18f85352
Justin Burgin <justinburgin@pursuit.org>
2020-02-02 16:41:37 -0500

    skeleWork db work

--- app/src/main/java/org/quietlip/guesswhatwho/db/DatabaseContract.java
+++ app/src/main/java/org/quietlip/guesswhatwho/db/DatabaseContract.java
@@ -7,16 +7,20 @@ public class DatabaseContract {
 
     public static class GameEntry implements BaseColumns {
         public static final String TABLE_NAME = "mystery_Game";
-        public static final String COLUMN_NAME_ROUND = "round";
         public static final String COLUMN_NAME_SCORE = "score";
         public static final String COLUMN_NAME_GUESS = "guess";
+        public static final String COLUMN_NAME_GUESS_CORRECT = "guess_correct";
     }
 
     static final String SQL_CREATE_ENTRIES = "CREATE TABLE " +
             GameEntry.TABLE_NAME + " (" + GameEntry._ID + " INTEGER PRIMARY KEY," +
-            GameEntry.COLUMN_NAME_ROUND + " TEXT," +
-            GameEntry.COLUMN_NAME_SCORE + " INTEGER," +
-            GameEntry.COLUMN_NAME_GUESS + " INTEGER)";
+            GameEntry.COLUMN_NAME_GUESS + " INTEGER," +
+            GameEntry.COLUMN_NAME_GUESS_CORRECT + " INTEGER, " +
+            // 0 (FALSE) && 1 (TRUE)
+            GameEntry.COLUMN_NAME_SCORE + "INTEGER)";
 
     static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + GameEntry.TABLE_NAME;
+
+    //col: key(round), guess, correct||incorrect, w/l ration
+
 }
